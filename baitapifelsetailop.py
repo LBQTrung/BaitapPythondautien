7adfb3148c2d4040bfc073019d3e2edf72ee3469
LBQTrung <91483513+LBQTrung@users.noreply.github.com>
2021-10-05 07:53:14 +0700

    Add files via upload

--- baitapifelsetailop.py
+++ baitapifelsetailop.py
@@ -1,11 +1,15 @@
 dtb = float(input("Nhap diem trung binh: "))
-if (dtb >= 9):
-    print("Xuat sac")
-elif (dtb >= 8):
-    print("Gioi")
-elif (dtb >= 7):
-    print("Kha")
-elif (dtb >= 5):
-    print("Trung binh")
+if (dtb >= 0):
+    if (dtb >= 9):
+        print("Xuat sac")
+    elif (dtb >= 8):
+        print("Gioi")
+    elif (dtb >= 7):
+        print("Kha")
+    elif (dtb >= 5):
+        print("Trung binh")
+    else:
+        print("Yeu")
 else:
-    print("Yeu")
+    print("Nhap diem khong hop le")
+
